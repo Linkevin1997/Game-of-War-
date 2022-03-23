986c4a7ba0a945f8ead23b1611572b47aa32d9b6
Kevin Lin <kevinlin@Kevins-MacBook-Pro.local>
2022-03-23 11:17:28 -0400

    Updating Readme

--- challenge.js
+++ challenge.js
@@ -1,6 +1,6 @@
 //declare variable
 const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
-const cardNum = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King','Ace'];
+const cardNum = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
 
 
 
@@ -9,35 +9,65 @@ class Deck {
     this.cards = [];
   }
 }
-  
-class Cards {
-  constructor(suit, card, value) {
+
+class Card {
+  constructor(suit, num, value) {
     this.suit = suit;
-    this.value = value; 
-    this.card = card;
+    this.num = num;
+    this.value = value;
   }
 }
 
 const fullDeck = new Deck()
 
-for (let i = 0; i < suits.length; i += 1){
-  for (let j = 0; j < cardNum.length; j += 1){
-    fullDeck.cards.push(new Cards(suits[i], cardNum[j], j));
+for (let i = 0; i < suits.length; i += 1) {
+  for (let j = 0; j < cardNum.length; j += 1) {
+    fullDeck.cards.push(new Card(suits[i], cardNum[j], j));
   }
 }
-console.log(fullDeck)
+
 
 //shuffle deck
+fullDeck.cards.sort((a, b) => Math.random() - .5)
 
-//distribute 2 decks
+//distribute 2 decks:
+const playerOne = fullDeck.cards.splice(0, 26)
+const playerTwo = fullDeck.cards
 
 //compare cards
-  //if p1 wins
-    //take both cards
-  //if p2 wins
-    //take both cards
-  //tie condition
-    //skip 3 cards
+//if p1 wins
+//take both cards
+//if p2 wins
+//take both cards
+//tie condition
+//skip 3 cards
+function compare() {
+  let cardOne = playerOne.pop()
+  let cardTwo = playerTwo.pop()
+  if (cardOne.value > cardTwo.value) {
+    playerOne.unshift(cardOne, cardTwo)
+  } else if (cardTwo.value > cardOne.value) {
+    playerTwo.unshift(cardTwo, cardOne)
+  } else {
+    console.log('tie');
+  }
+}
+compare()
+console.log(playerOne.length)
 
-    
+
+//seperate function to handle the tie: //tie condition
+//remove last three cards from both player deck. Put them in a constant, compare next card. 
+//winner get three cards.
+function handleTie() {
+  let oneRemove = playerOne.splice(playerOne.length - 3, 3)
+  let twoRemove = playerTwo.splice(playerTwo.length - 3, 3)
+  let twoOne = playerTwo.pop()
+  let oneOne = playerOne.pop()
+  if (oneRemove > twoRemove) {
+    playerOne.
+  }
+}
+
+//create function to play the game.
 
